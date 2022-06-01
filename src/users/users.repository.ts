0d2a35820c173748d7ab9689b8cef62716765a50
Brenda <breeeendaqn@gmail.com>
2022-05-31 21:53:30 -0300

    fix: add injectable and try/catch to repository

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,13 +1,19 @@
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma/prisma.service';
 import { User } from './entities/user.entity';
 
+@Injectable()
 export class UsersRepository {
   constructor(private prisma: PrismaService) {}
 
   async save(user: User) {
-    const response = this.prisma.user.create({
-      data: user,
-    });
-    return response;
+    try {
+      const response = this.prisma.user.create({
+        data: user,
+      });
+      return response;
+    } catch (error) {
+      throw new Error('Error in users repository')
+    }
   }
 }
